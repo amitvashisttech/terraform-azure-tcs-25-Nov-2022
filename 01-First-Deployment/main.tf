af3e8002447d2afe1e90f5f6a761c263948965e2
Amit Vashist <amitvashist7@outlook.com>
2022-11-25 14:37:44 +0000

    Terraform - 00-Setup

---
+++ 01-First-Deployment/main.tf
@@ -0,0 +1,12 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  #name     = "Blue-RG"
+  name     = "Green-RG"
+  location = "eastus"
+}
