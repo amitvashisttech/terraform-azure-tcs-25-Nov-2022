13053c67f7423e13dab8503965742f7d407f47c9
Amit Vashist <amitvashist7@outlook.com>
2022-11-25 14:47:29 +0000

    Terraform - 00-Setup

--- 01-First-Deployment/main.tf
+++ 01-First-Deployment/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  #name     = "Blue-RG"
-  name     = "Green-RG"
+  name     = "Blue-RG"
+  #name     = "Green-RG-AV"
   location = "eastus"
 }
