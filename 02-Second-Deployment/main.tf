eff26c0eb35d8a09edfc159a6dff49b67253d43d
AV <amitvashist7@outlook.com>
2022-11-26 04:55:54 +0000

    02-Second-Deploymen

---
+++ 02-Second-Deployment/main.tf
@@ -0,0 +1,33 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "Blue-RG"
+  location = "eastus"
+
+  tags = { 
+    Owner = "Amit Vashist" 
+    Team = "DevOps"
+    Project = "TF Automation"
+  }
+  
+}
+
+
+
+# Create a resource group
+resource "azurerm_resource_group" "green" {
+  name     = "Green-RG"
+  location = "eastus"
+
+  tags = { 
+    Owner = "Amit Vashist" 
+    Team = "DevOps"
+    Project = "TF Automation"
+  }
+  
+}
