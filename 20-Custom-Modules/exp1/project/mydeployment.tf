119055b1fbc82b35732cd2c7d9bf009b9551287c
AV <amitvashist7@outlook.com>
2022-12-13 12:58:03 +0000

    23-Virtual-Machine-SSH

--- 20-Custom-Modules/exp1/project/mydeployment.tf
+++ 20-Custom-Modules/exp1/project/mydeployment.tf
@@ -11,7 +11,7 @@ module "network" {
   my_loc             = "eastus"
   my_vpc_cidr        = ["192.168.0.0/16"]
   my_vpc_subnet_cidr = ["192.168.1.0/24"]
-  my_vm_count        = 2
+  my_vm_count        = 3
 
 }
 
@@ -20,7 +20,7 @@ module "compute" {
   source             = "../modules/compute"
   my_rg_name         = "devops"
   my_loc             = "eastus"
-  my_vm_count        = 2
+  my_vm_count        = 3
   my_nic_id          = module.network.my_nic 
 }
 
